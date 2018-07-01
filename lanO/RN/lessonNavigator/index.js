0f16c664fcdc0db0828d3f0d75f05fe8aaa7f0ea
李留住 <liliuzhu@renrenche.com>
2018-07-02 02:21:21 +0800

    navigator传值

--- lanO/RN/lessonNavigator/index.js
+++ lanO/RN/lessonNavigator/index.js
@@ -1,6 +1,18 @@
 import { AppRegistry } from 'react-native';
+/*
+* 应用程序由很多功能视图组成，一个应用中最重要的功能之一就是“导航”，在ReactNative中成为“路由router”
+* 使用导航器可以让你在应用的不同场景（页面）间进行切换
+*
+* 在ReactNative中，由两个实现导航功能的组件：Navigator和NavigatorIOS
+* Navigator支持安卓和Ios，NavigatorIOS支持IOS
+* NavigatorIOS比Navigator具有更多的属性和方法，在UI方面可以进行更多的设置，例如：backButtonIcon、
+* backButtonTitle，onLeftButtonPress等等，比较方便
+* 如果想实现更多的自定义设置，建议使Navigator
+* */
+
 // import App from './App';
 // 实现导航功能，页面切换
-import App from './navigator';
+// import App from './navigator';
 // 实现导航功能，传值
+import App from './passValue';
 AppRegistry.registerComponent('lessonNavigator', () => App);
---
+++ lanO/RN/lessonNavigator/passValue.js
@@ -0,0 +1,156 @@
+import React, { Component } from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    TextInput
+} from 'react-native';
+import { Navigator } from 'react-native-deprecated-custom-components';
+// 从前一个页面向后一个页面传值
+
+// 定义inputPage 输入框 、按钮
+class InputPage extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            // 记录输入框的值
+            content: ''
+        };
+
+    };
+    getInputContent(inputText){
+        // 将输入框的值进行记录
+        this.setState({
+            content: inputText
+        });
+        // this.state.content = inputText;
+    };
+    pushNextpage(){
+      // 退出下一个页面，并且传值
+        let nextRoute = {
+            component: DetailPage,
+            passProps:{
+                // 将输入值的内容传递给下一级页面
+                showText: this.state.content
+            }
+        }
+        this.props.navigator.push(nextRoute)
+    };
+    render(){
+        return (
+            <View style={inputStyle.container}>
+                <TextInput style={inputStyle.input}
+                    placeholder="请输入内容"
+                    onChangeText={this.getInputContent.bind(this)}/>
+                <TouchableOpacity style={inputStyle.btn} onPress={this.pushNextpage.bind(this)}>
+                    <Text>进入下一页</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+const inputStyle = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'red'
+    },
+    input: {
+        width: '80%',
+        height: 45,
+        marginRight: 25,
+        marginLeft: 25,
+        borderWidth:1,
+        borderColor:'black',
+        borderRadius: 4
+    },
+    btn: {
+        marginTop: 20,
+        height: 30,
+        borderWidth:1,
+        borderColor:'black',
+        padding: 5,
+        justifyContent:'center',
+        alignItems: 'center'
+    }
+});
+
+// DetailPage 显示为本 按钮
+
+class DetailPage extends Component{
+    constructor(prop) {
+        super(prop);
+    };
+    popFrontPage(){
+        // 返回上一级
+        this.props.navigator.pop();
+    };
+    render(){
+        return(
+            <View style={detailStyle.container}>
+                <Text style={detailStyle.text}>{this.props.showText}</Text>
+                <TouchableOpacity style={inputStyle.btn} onPress={this.popFrontPage.bind(this)}>
+                    <Text>返回上一页</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+const detailStyle = StyleSheet.create({
+   container: {
+       flex:1,
+       justifyContent: 'center',
+       alignItems: 'center',
+       backgroundColor:'white'
+   },
+    text: {
+       marginLeft:25,
+        marginRight:25,
+        padding:25,
+        backgroundColor: 'cyan',
+        fontSize:20,
+        textAlign:'center'
+    }
+});
+
+export default class LessonNavigator extends Component{
+    render(){
+        let rootRoute = {
+            component: InputPage,
+            // 存储需要传递的内容
+            passProps:{
+
+            }
+        };
+        return (
+            <View style={styles.container}>
+                <Navigator
+                    initialRoute={rootRoute}
+                    configureScene={(route) => {
+                        return Navigator.SceneConfigs.PushFromRight;
+                    }}
+                    renderScene={(route,navigator)=>{
+                        let Component = route.component;
+                        return (
+                            <Component
+                             navigator={navigator}
+                             route={route}
+                             {...route.passProps}
+                            />
+                        )
+                    }}
+                />
+            </View>
+        )
+    }
+};
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'yellow'
+    }
+});
