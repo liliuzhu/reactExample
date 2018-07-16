11037cdc7080ebc73389fc3601ea41b11b0d3521
liliuzhu <lliuzhu1992@163.com>
2018-07-17 01:25:48 +0800

    dianping首页

---
+++ dianpingApp/app/containers/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+
+class App extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = {
+            initDone: false
+        }
+    }
+    render() {
+        return (
+            <div>
+                {
+                    this.state.initDone
+                        ?this.props.children
+                        :<div>加载中...</div>
+                }
+            </div>
+        )
+    }
+    componentDidMount() {
+        setTimeout(_ => {
+            this.setState({
+                initDone: true
+            })
+        }, 1000)
+    }
+}
+
+export default App
